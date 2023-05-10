69bbfead8e3722a277a096e4456cb1b1abae03b9
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-05-10 19:10:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#Anykernel3
+# AnyKernel3 路径
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
+# 编译完成后内核名字
+FINAL_KERNEL_ZIP=whatwurst_KSU_A13_dibin.zip
 # 内核工作目录
 export KERNEL_DIR=$(pwd)
 # 内核 defconfig 文件
@@ -21,10 +23,10 @@ export SUBARCH=arm64
 # 只使用clang编译需要配置
 export LLVM=1
 export BUILD_INITRAMFS=1
-#Kernel Name
-FINAL_KERNEL_ZIP=whatwurst_KSU_A13_dibin.zip
+
 # ./build.sh 4
 
+#16为线程数，可以指定#
 TH_COUNT=16
 if [[ "" != "$1" ]]; then
         TH_NUM=$1
@@ -40,17 +42,22 @@ export DEF_ARGS="O=${OUT} \
 
 export BUILD_ARGS="-j${TH_COUNT} ${DEF_ARGS}"
 
-# make defconfig
+echo -e "$yellow**** 开始编译内核 ****$nocol"
 make ${DEF_ARGS} ${KERNEL_DEFCONFIG}
-
 make ${BUILD_ARGS}
 
+echo -e "$yellow**** 验证 Image.gz-dtb ****$nocol"
 ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+echo -e "$yellow**** 验证 AnyKernel3 目录 ****$nocol"
 ls $ANYKERNEL3_DIR
+echo -e "$yellow**** 清理 AnyKernel3 工作目录 ****$nocol"
 rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
 rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+echo -e "$yellow**** 复制 Image.gz-dtb 到 AnyKernel3 工作目录 ****$nocol"
 cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+echo -e "$yellow**** 正在打包内核为可刷入 Zip 文件 ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-mv $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /mnt/disk/kernelout
+echo -e "$yellow**** 复制打包好的 Zip 文件到指定的输出目录 ****$nocol"
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /mnt/disk/kernelout
 
